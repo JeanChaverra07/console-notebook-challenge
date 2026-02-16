79f0e16b178c548d894e513c7928c3967f08a494
Jean Paul <jeanpaularteagachaverra@gmail.com>
2026-02-16 16:36:48 -0500

    Definicion constantes

--- src/notebook.py
+++ src/notebook.py
@@ -1,5 +1,6 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
 
-class Nota:
-    def __init__(self, titulo, texto, importancia):
-        self.id = Nota.contador_id
+class Note:
+    HIGH: str = 'HIGH'
+    MEDIUM: str = 'MEDIUM'
+    LOW: str = 'LOW'
