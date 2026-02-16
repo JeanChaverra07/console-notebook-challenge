42064abbfa3418a70845f64d5e3a48f74e90a07a
Jean Paul <jeanpaularteagachaverra@gmail.com>
2026-02-16 16:27:43 -0500

    Primer paso definicion clase Nota

--- src/notebook.py
+++ src/notebook.py
@@ -1,1 +1,5 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+
+class Nota:
+    def __init__(self, titulo, texto, importancia):
+        self.id = Nota.contador_id
