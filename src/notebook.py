f83256fc155ede778cf30ece3f2b3dca37af9a37
Jean Paul <jeanpaularteagachaverra@gmail.com>
2026-02-16 16:52:08 -0500

    Agregar las etiquetas a el atributo Tag

--- src/notebook.py
+++ src/notebook.py
@@ -9,3 +9,20 @@ def __init__(self, code: str, title: str, text: str, importance: str):
     self.code : str = code
     self.title : str = title
     self.text : str = text
+    self.importance : str = importance
+    self.tags: list[str] = []
+
+def add_tag(self, tag: str) -> None:
+    if tag not in self.tags:
+        self.tags.append(tag)
+
+def __str__(self) -> str:
+    tags_str = ', '.join(self.tags) if self.tags else ("No tags")
+    return (
+        f"Code: {self.code}\n"
+        f"Title: {self.title}\n"
+        f"Text: {self.text}\n"
+        f"Importance: {self.importance}\n"
+        f"Creation date: {self.creation_date}\n"
+        f"Tags: {tags_str}"
+    )
